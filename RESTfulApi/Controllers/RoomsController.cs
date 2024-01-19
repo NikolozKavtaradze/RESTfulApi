f30c07dfc0217ae6ec66e869aa8356b98ddfc913
NikolozKavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-01-19 10:04:34 -0800

    Add project files.

---
+++ RESTfulApi/Controllers/RoomsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTfulApi.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        { 
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RESTfulApi/Controllers/RootController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTfulApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        [ProducesResponseType(200)]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
